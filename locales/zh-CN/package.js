5966246e16ed71b7eea57a764238a92906625668
gockxml <hustgock@gmail.com>
2018-05-29 19:00:13 +0800

    first version

---
+++ locales/zh-CN/package.js
@@ -0,0 +1,13 @@
+module.exports = {
+  description: '二维码插件',
+  actionProviders: [
+    {
+      name: "生成二维码",
+      description: "生成二维码"
+    },
+    {
+      name: "识别二维码",
+      description: "识别生成二维码"
+    },
+  ]
+}
